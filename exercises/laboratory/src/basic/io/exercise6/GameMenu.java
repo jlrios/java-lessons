4780f3803eb49ab2f39319f160c0803adafe504b
José Ríos <rdjl5791@gmail.com>
2026-05-26 03:13:59 -0600

    feat: add exercise 6. Basic text menu

--- exercises/laboratory/src/basic/io/exercise6/GameMenu.java
+++
@@ -1,7 +0,0 @@
-package basic.io.exercise6;
-
-public class GameMenu {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ exercises/laboratory/src/basic/io/exercise6/OpeningMenu.java
@@ -0,0 +1,15 @@
+package basic.io.exercise6;
+
+import java.util.Scanner;
+
+public class OpeningMenu {
+    public static void main(String[] args) {
+        System.out.println("===== O P E N I N G   M E N U =====");
+        System.out.println("\t[O] Open a document\t\t\t\t[D] Delete a document");
+        System.out.println("\t[N] Open a non document\t\t\t[T] Protect a document");
+        System.out.println("\t[P] Print a file\t\t\t\t[R] Rename a document");
+        System.out.println("\t[H] Help\t\t\t\t\t\t[C] Copy document");
+        System.out.println("\t[E] Exit\t\t\t\t\t\t[M] Run terminal command");
+        System.out.print("Enter choice: ");
+    }
+}
